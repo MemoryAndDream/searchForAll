48b28316b8722683c4ce49899d6aa4bb652111b1
meng_zhihao <meng93914@gmail.com>
2017-08-21 23:15:55 +0800

    up

--- searchForAll/crawler/extractors/baidu.py
+++ searchForAll/crawler/extractors/baidu.py
@@ -9,8 +9,8 @@ def process(url):
 	urlinsfos=[]
 	#urlinfo1={"url":"http://www.baidu.com/link?url=966OdUyxuwFJoAYx_XGYq7_FiVLcej4qEA3Q84e-lLAtLPRGGHA6tsNFNsTN9zka&wd=&eqid=a64931cc000026c3000000035994fd9e","title":"python Django教程 之模板渲染、循环、条件判断、常用的..._博客园","info":'在 W3School,您将找到许多可以在线编辑并测试的 jQuery 实例。 jQuery 实例jQuery 参考手册 在W3School,您将找到包含所有 jQuery 对象和函数的完整参考手册。 jQuery...'}
 	page = ct.crawlerTool.getPage(url).replace('·','')
-	print page
-	print url
+	#print page
+	#print url
 	segments = ct.crawlerTool.getXpath('//div[@class="result c-container "]',page)#这个xpath可以过滤掉很多广告。。
 	#print segments
 	for segment in segments:
@@ -20,7 +20,7 @@ def process(url):
 			urlinfo['url']= ct.crawlerTool.getXpath('//a[1]/@href',segment)[0]
 			urlinfo['title'] = ct.crawlerTool.getXpath('//a[1]/text()', segment.replace('<em>','').replace('</em>',''))[0]
 			urlinfo['info'] = ct.crawlerTool.getXpath('//div[@class ="c-abstract"]/text()', segment.replace('<em>','').replace('</em>',''))[0]
-			print urlinfo['url'],urlinfo['title'],urlinfo['info']
+			#print urlinfo['url'],urlinfo['title'],urlinfo['info']
 			#info里有分隔符的时候出错
 			urlinsfos.append(urlinfo)
 		except:
--- searchForAll/crawler/extractors/bing.py
+++ searchForAll/crawler/extractors/bing.py
@@ -9,7 +9,7 @@ import sys
 #相对导入不能超过最高层
 def process(url):
 	urlinsfos=[]
-	page = ct.crawlerTool.getPage(url)
+	page = ct.crawlerTool.getPage(url).replace('&#183;','')#print HTMLParser().unescape('&#183;').encode('unicode-escape').decode('string_escape')是乱码
 	#print page
 	segments = ct.crawlerTool.getXpath('//li[@class="b_algo"]',page)#这个xpath可以过滤掉很多广告。。
 	#print segments
@@ -17,11 +17,13 @@ def process(url):
 		#print segment
 		urlinfo={}
 		urlinfo['url']= ct.crawlerTool.getXpath('//h2/a[1]/@href',segment)[0]
-		title = HTMLParser().unescape(ct.crawlerTool.extractorText(ct.crawlerTool.getXpath('//h2/a[1]', segment)[0])).encode('unicode-escape').decode('string_escape')
+
+		title = HTMLParser().unescape(ct.crawlerTool.extractorText(ct.crawlerTool.getXpath('//h2/a[1]', segment)[0])).encode('unicode-escape').decode('string_escape')#好像不转str格式后面输出是乱码S
 		#print title,HTMLParser().unescape(title)
-		#print ct.crawlerTool.getXpath('//h2/a[1]', segment)
+		#print ct.crawlerTool.getXpath('//h2/a[1]', segment)#解码后&#183;好像变乱码了
 		urlinfo['title'] = title
 		urlinfo['info'] = HTMLParser().unescape(ct.crawlerTool.extractorText(ct.crawlerTool.getXpath('//div[@class="b_caption"]', segment)[0])).encode('unicode-escape').decode('string_escape')
+		print urlinfo['url'], urlinfo['title'], urlinfo['info']
 		urlinsfos.append(urlinfo)
 	return urlinsfos
 
