d9a1db6a1fd619e0adedf30169bc8ec02ff788e8
MemoryAndDream <312141830@qq.com>
2017-10-31 15:53:40 +0800

    up

--- searchForAll/crawler/extractors/githubReposity.py
+++ searchForAll/crawler/extractors/githubReposity.py
@@ -22,7 +22,7 @@ def process(keyword, page):
 		#print segment
 		try:
 			urlinfo={}
-			urlinfo['url']= 'https://github.com'+ct.crawlerTool.getXpath('//h3/a/@href',segment)[0]
+			urlinfo['url']= ct.crawlerTool.getXpath('//h3/a/@href',segment)[0]
 			urlinfo['title'] = ct.crawlerTool.extractorText(ct.crawlerTool.getXpath('//h3//text()', segment))
 			urlinfo['info'] = ct.crawlerTool.extractorText(ct.crawlerTool.getXpath('//p//text()', segment))
 
